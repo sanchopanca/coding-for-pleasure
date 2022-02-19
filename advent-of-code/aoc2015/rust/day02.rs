8744f8f107802e5bb853fee63dc4775fcf3e34f0
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2022-02-19 13:50:18 +0100

    aoc 2015 in rust day 2

---
+++ advent-of-code/aoc2015/rust/day02.rs
@@ -0,0 +1,33 @@
+use std::cmp;
+use std::fs;
+
+
+fn paper(l: i32, w: i32, h: i32) -> i32 {
+    let s1 = l * w;
+    let s2 = w * h;
+    let s3 = h * l;
+    2 * s1 + 2 * s2 + 2 * s3 + cmp::min(s1, cmp::min(s2, s3))
+}
+
+fn ribbon(l: i32, w: i32, h: i32) -> i32 {
+    let p1 = 2 * (l + w);
+    let p2 = 2 * (w + h);
+    let p3 = 2 * (h + l);
+    cmp::min(p1, cmp::min(p2, p3)) + l * w * h
+}
+
+fn main() {
+    let dims = fs::read_to_string("../input/02.txt").unwrap();
+    let mut p = 0;
+    let mut r = 0;
+    for dim in dims.lines() {
+        let v: Vec<_> = dim.split("x").collect();
+        let l: i32 = v[0].parse().unwrap();
+        let w: i32 = v[1].parse().unwrap();
+        let h: i32 = v[2].parse().unwrap();
+        p += paper(l, w, h);
+        r += ribbon(l, w, h);
+    }
+    println!("{}", p);
+    println!("{}", r);
+}
