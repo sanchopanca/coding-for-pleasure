a0c40284aeb0ce82079cfc1a4fc81c6b62a22c68
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2022-02-19 12:36:13 +0100

    aoc 2015 in rust day 1

---
+++ advent-of-code/aoc2015/rust/day01.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+fn main() {
+    let directions = fs::read_to_string("../input/01.txt").unwrap();
+    let mut floor = 0;
+    let mut found = false;
+    for (i, d) in directions.chars().enumerate() {
+        if d == '(' {
+            floor += 1;
+        } else {
+            floor -= 1;
+        }
+        if floor == -1 && !found {
+            println!("part2: {}", i + 1);
+            found = true;
+        }
+    }
+    println!("part1: {}", floor);
+}
