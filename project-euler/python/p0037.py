d978c15ebb631e02a7dfa568a310a402fe04616a
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2022-06-03 13:15:55 +0200

    finish project euler 37

--- project-euler/python/p0037.py
+++ project-euler/python/p0037.py
@@ -11,7 +11,7 @@ def sieve(n):
 primes = sieve(1_000_000)
 
 
-def is_truncatable_prime(n):
+def is_truncatable_prime_right(n):
     if n < 10:
         return False
     n *= 10
@@ -20,12 +20,25 @@ def is_truncatable_prime(n):
             return False
     return True
 
+
+def is_truncatable_prime_left(n):
+    if n < 10 or n not in primes:
+        return False
+    while n := int('0' + str(n)[1:]):
+        if n not in primes:
+            return False
+    return True
+
+
+def is_truncatable_prime(n):
+    return is_truncatable_prime_right(n) and is_truncatable_prime_left(n)
+
+
 c = 0
 s = 0
 for p in primes:
     if is_truncatable_prime(p):
         c += 1
         s += p
-print(c)
 assert(c == 11)
 print(s)
