5dc111b229b585379a7e3fc424e3d8c6ed2ea01a
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2023-12-01 08:30:52 +0100

    aoc 2023 day 1
    Learned the hard way that find_iter() only considers non-overlapping
    matches.
    
    Still not sure what is the best way to find the last match.
    The manually reversing regex looks silly.

---
+++ advent-of-code/aoc2023/rust/src/bin/day01.rs
@@ -0,0 +1,61 @@
+use aoc2023::*;
+
+fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
+    let input = read_input_to_char_vectors(1);
+    let mut sum = 0;
+    for line in input {
+        let first_digit = line.iter().find(|x| x.is_ascii_digit()).unwrap();
+        let last_digit = line.iter().rev().find(|x| x.is_ascii_digit()).unwrap();
+
+        let number = 10 * first_digit.to_digit(10).unwrap() + last_digit.to_digit(10).unwrap();
+        sum += number;
+    }
+    println!("{sum}");
+}
+
+fn part2() {
+    let input = read_input_to_lines(1);
+    let re1 = regex::Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let re2 = regex::Regex::new(r"(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)").unwrap();
+
+    let mut sum = 0;
+    for line in input {
+        let reversed = line.chars().rev().collect::<String>();
+        let first_match = re1.find(&line).unwrap().as_str();
+        let last_match = re2.find(&reversed).unwrap().as_str();
+
+        let number = to_number(first_match) * 10 + to_number(last_match);
+
+        sum += number;
+    }
+    println!("{sum}");
+}
+
+fn to_number(s: &str) -> u32 {
+    match s {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        "eno" => 1,
+        "owt" => 2,
+        "eerht" => 3,
+        "ruof" => 4,
+        "evif" => 5,
+        "xis" => 6,
+        "neves" => 7,
+        "thgie" => 8,
+        "enin" => 9,
+        _ => s.parse::<u32>().unwrap(),
+    }
+}
---
+++ advent-of-code/aoc2023/rust/src/lib.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+pub fn read_input_to_string(day: u8) -> String {
+    fs::read_to_string(format!("../input/{:02}.txt", day))
+        .unwrap()
+        .trim()
+        .to_owned()
+}
+
+pub fn read_input_to_lines(day: u8) -> Vec<String> {
+    fs::read_to_string(format!("../input/{:02}.txt", day))
+        .unwrap()
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.trim().to_owned())
+        .collect()
+}
+
+pub fn read_input_to_char_vectors(day: u8) -> Vec<Vec<char>> {
+    read_input_to_lines(day)
+        .iter()
+        .map(|s| s.chars().collect())
+        .collect()
+}
+
+pub fn transpose<T>(v: &Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Clone,
+{
+    assert!(!v.is_empty());
+    (0..v[0].len())
+        .map(|i| v.iter().map(|inner| inner[i].clone()).collect::<Vec<T>>())
+        .collect()
+}
