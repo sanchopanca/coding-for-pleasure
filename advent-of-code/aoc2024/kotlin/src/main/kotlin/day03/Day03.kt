acf2e6bc109f0969f2d8058b407c51aa532236ae
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2024-12-03 08:32:58 +0100

    Refactor day 3

--- advent-of-code/aoc2024/kotlin/src/main/kotlin/day03/Day03.kt
+++ advent-of-code/aoc2024/kotlin/src/main/kotlin/day03/Day03.kt
@@ -11,15 +11,36 @@ fun main() {
 
 fun part1() {
     val lines = File("../input/03.txt").readLines()
-    val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
+    val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
     var result = 0
     for (line in lines) {
         val matches = regex.findAll(line);
         for (match in matches) {
-            result += match.groupValues[1].toInt() * match.groupValues[2].toInt()
+            val (a, b) = match.destructured
+            result += a.toInt() * b.toInt()
         }
     }
     println(result)
+}
+
+fun part2() {
+    val regex = """(?<mul>mul\((\d{1,3}),(\d{1,3})\))|(?<do>do\(\))|(?<dont>don't\(\))""".toRegex()
+    var isProcessingEnabled = true
+    var totalSum = 0
+
+    File("../input/03.txt").forEachLine { line ->
+        regex.findAll(line).forEach { match ->
+            when {
+                match.groups["mul"] != null && isProcessingEnabled -> {
+                    val (_, a, b) = match.destructured
+                    totalSum += a.toInt() * b.toInt()
+                }
+                match.groups["do"] != null -> isProcessingEnabled = true
+                match.groups["dont"] != null -> isProcessingEnabled = false
+            }
+        }
+    }
+    println(totalSum)
 }
 
 fun part2b() {  // doesn't work and i don't understand why
@@ -28,46 +49,12 @@ fun part2b() {  // doesn't work and i don't understand why
     val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
     val remover = "don't\\(\\)(?!do\\(\\)).*?(do\\(\\)|\$)".toRegex()
     var result = 0
-        while (remover.containsMatchIn(line)) {
-            line = line.replace(remover, "")
-            println("!!!$line")
-        }
-        val matches = regex.findAll(line);
-        for (match in matches) {
-            result += match.groupValues[1].toInt() * match.groupValues[2].toInt()
-        }
-    println(result)
-}
-
-fun part2() {
-    val lines = File("../input/03.txt").readLines()
-    val regex = "(mul\\((\\d{1,3}),(\\d{1,3})\\))|(do\\(\\))|(don't\\(\\))".toRegex()
-
-    var result = 0
-    var enabled = true;
-    for (line in lines) {
-
-
-        val matches = regex.findAll(line)
-        for (match in matches) {
-            if (enabled && match.groupValues[1] != "") {
-                result += match.groupValues[2].toInt() * match.groupValues[3].toInt()
-            }
-
-            if (match.groupValues[4] != "") {
-                enabled = true
-            }
-
-            if (match.groupValues[5] != "") {
-                enabled = false
-            }
-
-        }
+    while (remover.containsMatchIn(line)) {
+        line = line.replace(remover, "")
     }
-    println(result)
+    val matches = regex.findAll(line);
+    for (match in matches) {
+        result += match.groupValues[1].toInt() * match.groupValues[2].toInt()
+    }
+    println("Wrong: $result")
 }
-
-// 98833043 wrong
-// 71007585 wrong
-// 110659571 wrong
-// 94810037 wrong
